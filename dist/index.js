2ce8a922b65ee7c25108bbef64d6bd63d439c2d6
dmitrydolgiy <dmitry.dolgiy@gmail.com>
2018-09-14 13:02:12 +0300

    training

--- dist/index.js
+++ dist/index.js
@@ -8,24 +8,25 @@ var _config = require('../config/config');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var bot = new _nodeTelegramBotApi2.default(_config.CONFIG.TOKEN, { webHook: { port: _config.CONFIG.PORT } });
+var bot = new _nodeTelegramBotApi2.default(_config.CONFIG.TOKEN, { polling: true });
 
-bot.setWebHook(_config.CONFIG.URL + '/bot' + _config.CONFIG.TOKEN);
+// bot.setWebHook(`${CONFIG.URL}/bot${CONFIG.TOKEN}`);
 
-bot.on('i', function (msg) {
-    var id = msg.chat.id,
-        _msg$from = msg.from,
-        first_name = _msg$from.first_name,
-        last_name = _msg$from.last_name;
-
-    bot.sendMessage(id, 'https://s.tcdn.co/176/ee2/176ee215-2504-3415-913a-095c7db4b253/1.png');
-});
 var options = {
-    reply_markup: JSON.stringify({
-        inline_keyboard: [[{ text: 'Хуево', callback_data: 'data 1' }], [{ text: 'Хуево', callback_data: 'data 1' }], [{ text: 'Хуево', callback_data: 'data 1' }], [{ text: 'Хуево', callback_data: 'data 1' }], [{ text: 'Хуево', callback_data: 'data 1' }], [{ text: 'Хуево', callback_data: 'data 1' }], [{ text: 'Хуево', callback_data: 'data 1' }], [{ text: 'Хуево', callback_data: 'data 1' }], [{ text: 'Хуево', callback_data: 'data 1' }], [{ text: 'Хуево', callback_data: 'data 1' }], [{ text: 'Хуево', callback_data: 'data 1' }], [{ text: 'Очень хуево', callback_data: 'data 2' }], [{ text: 'Крайне неутешительно', callback_data: 'data 3' }]]
-    })
+    reply_markup: {
+        inline_keyboard: [[{ text: 'Хуево', callback_data: 'data 1' }, { text: 'Takoe', callback_data: 'data 1' }], [{ text: 'Очень хуево', callback_data: 'data 2' }], [{ text: 'Крайне неутешительно', callback_data: 'data 3' }]]
+    }
 };
 
-bot.on('message', function (msg) {
-    bot.sendMessage(msg.chat.id, 'Выберите любую кнопку:', options);
+bot.on('message', function (_ref) {
+    var id = _ref.chat.id;
+
+    bot.sendMessage(id, 'Выбери уже хоть что-нибудь:', options);
+});
+
+bot.on('callback_query', function (query) {
+    console.log(JSON.stringify(query));
+    bot.answerCallbackQuery(query.id, JSON.stringify(query, null, 4)).catch(function (msg) {
+        return bot.sendSticker(msg.id, 'https://s.tcdn.co/4f6/11b/4f611b70-8750-3da1-ac75-bdea6dc88a9b/12.png');
+    });
 });
--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { CONFIG } from '../config/config';
+import fs from 'fs';
+import axios from 'axios';
 
-const bot = new TelegramBot(CONFIG.TOKEN, { webHook: { port: CONFIG.PORT } });
+const bot = new TelegramBot(CONFIG.TOKEN, { polling: true });
 
-bot.setWebHook(`${CONFIG.URL}/bot${CONFIG.TOKEN}`);
+// bot.setWebHook(`${CONFIG.URL}/bot${CONFIG.TOKEN}`);
 
-bot.on('i', msg => {
-    const { chat: { id }, from: { first_name, last_name } } = msg;
-    bot.sendMessage(id, 'https://s.tcdn.co/176/ee2/176ee215-2504-3415-913a-095c7db4b253/1.png');
-});
 const options = {
-    reply_markup: JSON.stringify({
-        inline_keyboard: [
-            [{ text: 'Хуево', callback_data: 'data 1' }],
-            [{ text: 'Очень хуево', callback_data: 'data 2' }],
-            [{ text: 'Крайне неутешительно', callback_data: 'data 3' }]
+    reply_markup: {
+        resize_keyboard: true,
+        request_location: true,
+        keyboard: [
+            [{ text: 'dmitry.dolgiy', callback_data: 'dmitry.daolgiy' }, {
+                text: 'Do nothing',
+                callback_data: 'Just data'
+            }],
         ]
-    })
+    }
 };
 
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Выберите любую кнопку:', options);
+bot.on('message', ({ chat: { id } }) => {
+    bot.sendMessage(id, 'Выбери уже хоть что-нибудь:', options);
+});
+
+bot.on('message', ({ text, chat: { id } }) => {
+    axios.get(`https://www.instagram.com/web/search/topsearch/?context=blended&query=${text}&rank_token=0.7305849633342247&include_reel=false`)
+        .then(res => {
+            axios.get(`https://api.instagram.com/v1/users/${res.data.users[0].user.pk}/media/recent?access_token=297728826.1677ed0.2b33b56306b94a70b652eb923bf74538`)
+                .then(({ data }) => {
+                    const imageCollection = data.data
+                        .map(({ images }) => ({ media: images.standard_resolution.url, type: 'photo' }));
+                    const [one, two, ...rest] = imageCollection;
+                    bot.sendMediaGroup(id, rest);
+                });
+        });
 });
